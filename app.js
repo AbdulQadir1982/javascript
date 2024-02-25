7f82ab6da4a018eb71472117dea749f5fca94d37
AbdulQadir <117006012+AbdulQadir1982@users.noreply.github.com>
2024-02-25 23:43:46 +0500

    chapter-1

---
+++ app.js
@@ -0,0 +1,25 @@
+// Q-1
+
+var a = "Plaese Enter a Password";
+
+alert(a);
+document.write('<div class="list-item">' + a + '</div>');
+document.write('<hr>');
+
+// Q-2
+var a = "Welcome To JS Land ";
+var b = "Happy Codding";
+var massege = a + "\n" + b;
+
+alert(massege);
+document.write('<div class="list-item">' + massege + '</div>');
+document.write('<hr>');
+
+// Q-4
+alert(a);
+alert(b);
+
+// Q-5
+var a = "Hello... I can Js throw my web browser's console";
+alert(a);
+console.log(a);
