5041a31e56230203d176895d27f47317cc952e26
AbdulQadir <117006012+AbdulQadir1982@users.noreply.github.com>
2024-02-29 22:44:48 +0500

    Add files via upload

---
+++ Calculaor/app.js
@@ -0,0 +1,26 @@
+// Sample Input  (User input)
+// =====================
+
+let num1 = +prompt("already",10);
+let num2 = +prompt("already",5);
+console.log("Values of variables " + num1,num2);
+
+// sum of two numbers
+let sum = num1 + num2;
+console.log("sum result " + sum);
+
+// Substraction
+let sub = num1 - num2;
+console.log("substraction result " + sub);
+
+// multipliction
+let mult = num1 * num2;
+console.log("multiplication result " + mult);
+
+// Division
+let divi = num1 / num2;
+console.log("division result " + divi);
+
+// Reminder
+let rimind = num1 % num2;
+console.log("reminder result " + rimind);
