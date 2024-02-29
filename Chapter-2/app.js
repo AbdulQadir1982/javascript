36e0b301ef1964d11f3224bf949263342ef0080b
AbdulQadir <117006012+AbdulQadir1982@users.noreply.github.com>
2024-02-29 21:42:24 +0500

    Add files via upload

---
+++ Chapter-2/app.js
@@ -0,0 +1,64 @@
+// Q1. Declare a variable called username?
+// Answer
+let userName = "A-Qadir";
+
+// Q2.Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+var my_name = "AbdulQadr";
+
+// Q3.Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box
+// a) 
+var message;
+// b)
+message = "Hello World";
+// c)
+alert(message);
+
+// Q4.Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes
+var student_name = "Jhone Doe";
+var studentAge = "15 years old";
+var student_degree = "Certified mobile Application Development";
+alert(student_name);
+alert(studentAge);
+alert(student_degree);
+
+// Q5.Write a script to display the following alert using one JS
+// variable:
+var favorite = "Pizza";
+alert(favorite);
+favorite = "Pizz";
+alert(favorite);
+favorite = "piz";
+alert(favorite);
+favorite = "pi";
+alert(favorite);
+favorite = "p";
+alert(favorite);
+
+// Q6.Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+var email;
+email = "Qadri@gmal.com";
+alert("My email Is " + email);
+
+// Q7.Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book = "A smarter way to learn javascript";
+alert("i am trying to learn from the book " + book);
+
+// Q8.Write a script to display this in browser through JS
+document.write("<h1> Yeh! i can write HTML content Throw javascript </h1>");
+
+// Q9.Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
+document.write(design);
