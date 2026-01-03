d0c9929a0299c79e429f46e921b765c924e0e708
Qadir Dev <117006012+AbdulQadir1982@users.noreply.github.com>
2026-01-03 22:51:57 +0500

    Add files via upload

---
+++ Chapter-13-15/app.js
@@ -0,0 +1,46 @@
+// Q-1   Declare an empty array using JS literal notation to store
+// student names in future.
+
+function ans1() {
+    let studentName = ["web and App","python","web designing"];
+    console.log(studentName);
+
+}
+
+// Q-2 Declare an empty array using JS object notation to store
+// student names in future.
+
+function ans2() {
+
+    let studentData = new Array();
+    console.log(studentData);
+}
+
+// Q-3 Declare and initialized a string arrey
+
+function ans3() {
+    let students = ["Ahemed","sami","Ahetasham","AbdulQadir"];
+    console.log(students);
+}
+
+// Q-4 Declare and initialized a number arrey
+
+function ans4() {
+    let studentNumber = [1,2,3,4];
+    console.log(studentNumber);
+}
+
+// Q-5 Declare and initialized a boolean arrey
+
+function ans5() {
+    let is_student_present = [true,false,true,false];
+    console.log(is_student_present);
+}
+
+// Q-6 Declare and initialized a mixed arrey
+
+function ans6() {
+    let mixed = ["Ahemed","Age",40,false];
+    console.log(mixed);
+}
+
