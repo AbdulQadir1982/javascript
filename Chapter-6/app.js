0dfd92eb076276bfd720db17b631d793377c00fd
Qadir Dev <117006012+AbdulQadir1982@users.noreply.github.com>
2026-01-03 22:50:00 +0500

    Add files via upload

---
+++ Chapter-6/app.js
@@ -0,0 +1,9 @@
+// 1. Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
+function ques1() {
+    let num = 10;
+    document.write(`The value of num is ${num} <br/>`);
+    
+
+}
