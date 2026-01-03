9b389f51f87860fbbe9cc49a1485f718dd737b3a
Qadir Dev <117006012+AbdulQadir1982@users.noreply.github.com>
2026-01-03 22:52:29 +0500

    Add files via upload

---
+++ Chapter-17-20/app.js
@@ -0,0 +1,13 @@
+// Q-1 Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+function ans1() {
+
+let multiArray = [];
+
+multiArray.push([]);
+multiArray.push([]);
+multiArray.push([]);
+
+console.log(multiArray);
+}
